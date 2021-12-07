b9e0839d50372e3f677b54a5bc9eeacf08fb72a3
victoradan2015 <victoradan2015@gmail.com>
2021-12-07 04:39:09 -0600

    Comida Italiana terminada

--- app/src/main/java/com/example/myapplication/RepertorioRecetasItalia.java
+++ app/src/main/java/com/example/myapplication/RepertorioRecetasItalia.java
@@ -34,4 +34,49 @@ public class RepertorioRecetasItalia extends AppCompatActivity {
         startActivity(clickear);
     }
 
+    public void Lapolenta(View view) {
+        Intent clickear = new Intent(this, LaPolenta.class);
+        startActivity(clickear);
+    }
+
+    public void Lasana(View view) {
+        Intent clickear = new Intent(this, Lasana.class);
+        startActivity(clickear);
+    }
+
+    public void Minestrone(View view) {
+        Intent clickear = new Intent(this, Minestrone.class);
+        startActivity(clickear);
+    }
+
+    public void Ossobuco(View view) {
+        Intent clickear = new Intent(this, Ossobuco.class);
+        startActivity(clickear);
+    }
+
+    public void Pasta(View view) {
+        Intent clickear = new Intent(this, Pasta.class);
+        startActivity(clickear);
+    }
+
+    public void Pizza(View view) {
+        Intent clickear = new Intent(this, Pizza.class);
+        startActivity(clickear);
+    }
+
+    public void Ravioles(View view) {
+        Intent clickear = new Intent(this, Ravioles.class);
+        startActivity(clickear);
+    }
+
+    public void Risotto(View view) {
+        Intent clickear = new Intent(this, Risotto.class);
+        startActivity(clickear);
+    }
+
+    public void Spagetti(View view) {
+        Intent clickear = new Intent(this, Spagetti.class);
+        startActivity(clickear);
+    }
+
 }
