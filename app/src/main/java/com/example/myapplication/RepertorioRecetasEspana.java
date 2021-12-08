e8d8ac1dbb594fad95899ba8e332f03ef0a76afa
victoradan2015 <victoradan2015@gmail.com>
2021-12-08 07:13:04 -0600

    Colombia terminada

---
+++ app/src/main/java/com/example/myapplication/RepertorioRecetasEspana.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RepertorioRecetasEspana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repertorio_recetas_espana);
+    }
+}
