9c494b452fcd4aebd702ceec3d51eccabe12f575
victoradan2015 <victoradan2015@gmail.com>
2021-12-10 04:23:07 -0600

    Logo e interacciones cambidas

--- app/src/main/java/com/example/myapplication/Registro.java
+++ app/src/main/java/com/example/myapplication/Registro.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registro extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
+
+    public void Login(View view) {
+        Intent clickear = new Intent(this, Login.class);
+        startActivity(clickear);
+    }
+
+    public void Resgistrarme(View view) {
+        Intent clickear = new Intent(this, RepertorioPaises.class);
+        startActivity(clickear);
+    }
+
 }
